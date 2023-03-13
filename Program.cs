df4c42561eb8a1077c8d7920d195925625edb142
Tharushi Weerasinghe <tharushiu2000@gmail.com>
2023-03-13 16:29:14 +0530

    Operations

--- Program.cs
+++ Program.cs
@@ -10,7 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
+            Console.WriteLine("Enter the first number = ");
+            string number1;
+            number1 = Console.ReadLine();
+            int num1 = Convert.ToInt32(number1);
+
+
+            Console.WriteLine("Enter the second number = ");
+            string number2 = Console.ReadLine();
+            int num2 = Convert.ToInt32(number2);
+
+            int add = num1 + num2;
+            Console.WriteLine("Addition is " + add);
+
+            int sub = num1 - num2;
+            Console.WriteLine("Substraction is " + sub);
+
+            int multi = num1 * num2;
+            Console.WriteLine("Multipication is " + multi);
+
+            int dev = num1 / num2;
+            Console.WriteLine("Devition is " + dev);
+
             System.Console.ReadLine();
         }
     }
