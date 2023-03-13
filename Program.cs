ee6b1c65d279f25792ddd8f7218ebf1d04ad992b
Tharushi Weerasinghe <tharushiu2000@gmail.com>
2023-03-13 16:33:44 +0530

    Salary Calc

--- Program.cs
+++ Program.cs
@@ -10,27 +10,16 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first number = ");
-            string number1;
-            number1 = Console.ReadLine();
-            int num1 = Convert.ToInt32(number1);
+            Console.WriteLine("Enter the hourly rate of employee");
+            string hourlyRate = Console.ReadLine();
+            double hourlyRate_1 = Convert.ToDouble(hourlyRate);
 
+            Console.WriteLine("Enter the number of hours worked the employee");
+            string hoursworked = Console.ReadLine();
+            double hoursworked_1 = Convert.ToDouble(hoursworked);
 
-            Console.WriteLine("Enter the second number = ");
-            string number2 = Console.ReadLine();
-            int num2 = Convert.ToInt32(number2);
-
-            int add = num1 + num2;
-            Console.WriteLine("Addition is " + add);
-
-            int sub = num1 - num2;
-            Console.WriteLine("Substraction is " + sub);
-
-            int multi = num1 * num2;
-            Console.WriteLine("Multipication is " + multi);
-
-            int dev = num1 / num2;
-            Console.WriteLine("Devition is " + dev);
+            double grosspay = hourlyRate_1 * hoursworked_1;
+            Console.WriteLine("Gross pay of an employee is " + grosspay);
 
             System.Console.ReadLine();
         }
