3d29894d6ee93e52792b7e99d747dde8522fc80d
Tharushi Weerasinghe <tharushiu2000@gmail.com>
2023-03-13 16:48:36 +0530

    House rent Calculation

--- Program.cs
+++ Program.cs
@@ -10,16 +10,31 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the hourly rate of employee");
-            string hourlyRate = Console.ReadLine();
-            double hourlyRate_1 = Convert.ToDouble(hourlyRate);
+            string house;
+            string electricity;
+            string telephone;
+            string water;
 
-            Console.WriteLine("Enter the number of hours worked the employee");
-            string hoursworked = Console.ReadLine();
-            double hoursworked_1 = Convert.ToDouble(hoursworked);
+            Console.WriteLine("Please enter the monthly rent");
+            house = Console.ReadLine();
+            double house1 = Convert.ToDouble(house);
 
-            double grosspay = hourlyRate_1 * hoursworked_1;
-            Console.WriteLine("Gross pay of an employee is " + grosspay);
+            Console.WriteLine("Please enter the avareage electricity");
+            electricity = Console.ReadLine();
+            double electricity1 = Convert.ToDouble(electricity);
+
+            Console.WriteLine("Please enter the monthly telephone bill");
+            telephone = Console.ReadLine();
+            double telephone1 = Convert.ToDouble(telephone);
+
+            Console.WriteLine("Please enter the monthly water bill");
+            water = Console.ReadLine();
+            double water1 = Convert.ToDouble(water);
+
+            double totalExpenditure;
+            totalExpenditure = (house1 + electricity1 + telephone1 + water1) * 12;
+            Console.WriteLine("Total Expenditure for one year is " + totalExpenditure);
+
 
             System.Console.ReadLine();
         }
