869a6181275164d7d4e3d4f408b7412a80afd4e4
hasira09 <hasiramahel09@gmail.com>
2023-03-14 17:39:32 +0530

    Variables - New

--- Program.cs
+++ Program.cs
@@ -10,22 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Student Marks ");
-            string number1;
-            number1 = Console.ReadLine();
-            int marks = Convert.ToInt32(number1);
 
-            if (marks >= 75)
-                Console.WriteLine("your results is A");
-            else if (marks >= 65)
-                Console.WriteLine("your resuls is B");
+            // Variable Declaration
 
-            else if (marks >= 50)
-                Console.WriteLine("your results is C");
-            else if (marks >= 40)
-                Console.WriteLine("your results is S");
-            else
-                Console.WriteLine("your results is F");
+            string name; // words
+
+            int age; // whole numbers
+
+            float salary;
+            double savemoney; // decimals
+
+            char fueltype; // single letter (characters)
+
+            bool currencyStatus; // True or False
 
             System.Console.ReadLine();
         }
