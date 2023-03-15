8696c465e477be786436cedd8e578d32508d82ae
hasira09 <hasiramahel09@gmail.com>
2023-03-15 14:49:01 +0530

    If - Else Conditions

--- Program.cs
+++ Program.cs
@@ -10,36 +10,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            int num1 = 20;
-            int num2 = 30;
-            int num3 = 30;
-
-            /*
-            //Arithmetic Operations
-            Console.WriteLine(num1 + num2);
-            Console.WriteLine(num1 - num2);
-            Console.WriteLine(num1 * num2);
-            Console.WriteLine(num1 / num2);
-            Console.WriteLine(num1 % num2);
-            Console.WriteLine(num1++);
-            Console.WriteLine(num1--);
-            Console.WriteLine(num2--);
-            Console.WriteLine(num2++);
+            Console.Write("Enter Your Amount: ");
+            int number1 = Int16.Parse(Console.ReadLine());
             
 
-            //Assignment Operations 
-            Console.WriteLine(num1 == num2);
-            Console.WriteLine(num3 == num2);
-            Console.WriteLine(num1 != num2);
-            Console.WriteLine(num3 != num2);
-            Console.WriteLine(num1 > num2);
-            Console.WriteLine(num3 > num2);
-            */
+            if (number1 >= 90) 
+                Console.WriteLine("10 Royalty Rewards");
+            
+            else if (number1 >= 75)
+                Console.WriteLine("8 Royalty Rewards");
 
-            //Logical Operators
-            Console.WriteLine(num1 < num2 && num2 == num3);
-            Console.WriteLine(num1 < num2 && num2 == num3);
-            Console.WriteLine(!(num1 < num2));
+            else if (number1 >= 60)
+                Console.WriteLine("6 Royalty Rewards");
+
+            else if (number1 >= 45)
+                Console.WriteLine("4 Royalty Rewards");
+
+            else if (number1 >= 30)
+                Console.WriteLine("2 Royalty Rewards");
+
+            else
+                Console.WriteLine("0 Royalty Rewards");
 
             System.Console.ReadLine();
         }
