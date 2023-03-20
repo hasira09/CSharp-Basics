65790d3da133d4e35e704b9eb451c8902e2e55ab
Tharushi Weerasinghe <tharushiu2000@gmail.com>
2023-03-20 12:05:59 +0530

    Try & Catch

--- Program.cs
+++ Program.cs
@@ -10,32 +10,19 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            char ch;
-            Console.WriteLine("Enter an alphabet");
-            ch = Convert.ToChar(Console.ReadLine());
-
-            switch (Char.ToLower(ch))
+            try
             {
-                case 'a':
-                    Console.WriteLine("Vowel");
-                    break;
-                case 'e':
-                    Console.WriteLine("Vowel");
-                    break;
-                case 'i':
-                    Console.WriteLine("Vowel");
-                    break;
-                case 'o':
-                    Console.WriteLine("Vowel");
-                    break;
-                case 'u':
-                    Console.WriteLine("Vowel");
-                    break;
-                default:
-                    Console.WriteLine("Not a vowel");
-                    break;
+                var number = "1234";
+                byte  b = Convert.ToByte(number);
+                Console.WriteLine(number);
             }
-            System.Console.ReadLine();
+            catch(Exception)
+            {
+                Console.WriteLine("this number can't convert to byte");
+            }
+    Console.ReadLine();
+
+
         }
     }
 }
