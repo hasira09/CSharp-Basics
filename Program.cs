25dc30da2f97534cdb8742c4ec08c7493e60df51
hasira09 <hasiramahel09@gmail.com>
2023-03-20 23:12:19 +0530

    Arrays

--- Program.cs
+++ Program.cs
@@ -11,14 +11,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var hasira = new Person();
-            hasira.FirstName = "Hasira";
-            hasira.LastName = "Koswatta";
-            hasira.Introduce();
+            var numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(8, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Hasira", "Uma", "Tharu" };
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
+
             Console.ReadLine();
 
         }
