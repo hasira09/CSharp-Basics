a991fe112a5176ee9d2eec2d5fe443e99317d3d5
hasira09 <hasiramahel09@gmail.com>
2023-03-24 00:13:38 +0530

    Summarising Text

--- Program.cs
+++ Program.cs
@@ -12,43 +12,18 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            var fullName = "Tharushi Umayanga";
-            Console.WriteLine("Trim: '{0}'", fullName.Trim());
-            Console.WriteLine("ToUpper; '{0}'", fullName.Trim().ToUpper());
-
-            var index = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, index);
-            var lastName  = fullName.Substring(index + 1);
-            Console.WriteLine("First Name is: " +firstName);
-            Console.WriteLine("Last Name is: " + lastName);
-
-            Console.WriteLine();
-
-            var names = fullName.Split(' ');
-            Console.WriteLine("FirstName: " + names[0]);
-            Console.WriteLine("LastName: " + names[1]);
-
-            var replace = fullName.Replace("Tharushi", "Ummah");
-            var replace1 = fullName.Replace(' ', '-');
-
-            Console.WriteLine();
-            Console.WriteLine(replace);
-            Console.WriteLine(replace1);
-
-            if(String.IsNullOrWhiteSpace(" "))
-                Console.WriteLine("Invalid");
-
-            var str = "25";
-            var age = Convert.ToByte(str);
-            Console.WriteLine(age);
-
-            float price = 34.78f;
-            Console.WriteLine(price.ToString("C0"));
-
+            var sentence = "My Name is Hasira Mahel Koswatta. I'm a passionate developer with experience of HTML, CSS, JavaScript, Unity, C# etc. Currently I'm an Undergraduate of SLTC Research University.";
+            var summary = StringUtility.SummerizeText(sentence);
+            Console.WriteLine(summary);
             Console.ReadLine();
 
+
         }
+
+        
+        
                                       
         
     }
+    
 }
