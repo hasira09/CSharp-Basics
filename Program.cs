f720f37b95cd8bb92c00c563df82a876a257b741
Tharushi Weerasinghe <tharushiu2000@gmail.com>
2023-03-24 01:44:54 +0530

    Random_Class

--- Program.cs
+++ Program.cs
@@ -8,42 +8,23 @@ using System.Threading.Tasks;
 namespace ConsoleApp1
 {
 
-    class Program
+
+     
+    class program
     {
-        static void Main(string[] args)
-        {
-           var numbers = new List<int>() { 1, 2, 3, 4 };
-            numbers.Add(1);
-            numbers.AddRange(new int[3] { 5, 6, 7 });
+       static void Main(string[] args)
+        { 
+            var random= new Random();
+            const int passwordLength = 10;
 
-            foreach (var number in numbers)
-                Console.WriteLine(number);
+            var buffer = new char[passwordLength];
+            for (int i = 0; i < passwordLength; i++)
+                buffer[i] = (char) ('a' + random.Next(0,26));
 
-            //IndexOf
-            Console.WriteLine();
-            Console.WriteLine("Index of 1 is: "+ numbers.IndexOf(1));
-            Console.WriteLine("Last Index of 1 is: " + numbers.LastIndexOf(1));
-
-            //Count
-            Console.WriteLine("Count is: "+ numbers.Count);
-
-            //Remove
-            for (var i = 0; i < numbers.Count; i++)
-            {
-                if (numbers[i] == 1)
-                    numbers.Remove(numbers[i]);
-            }
-            foreach(var number in numbers)
-                Console.WriteLine(number);
-
-            //Clear
-            numbers.Clear();
-            Console.WriteLine("Count is: " + numbers.Count);
-
+            var password =  new string (buffer);
+            Console.WriteLine(password);
             Console.ReadLine();
-
         }
-                                      
         
     }
 }
