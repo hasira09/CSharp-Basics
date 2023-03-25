c82a97a1209451c1380ed7dedca1ec63cfa493ea
Tharushi Weerasinghe <tharushiu2000@gmail.com>
2023-03-25 23:29:07 +0530

    TimeSpan

--- Program.cs
+++ Program.cs
@@ -15,24 +15,30 @@ namespace ConsoleApp1
 
        static void Main(string[] args)
         {
+            //creting
 
-            var dateTime = new DateTime(2023, 3, 24);
-            var now = DateTime.Now;
-            var today = DateTime.Today;
+            var timeSpan = new TimeSpan(1, 2, 3);
+            var timeSpan1 = new TimeSpan(1, 0, 0);
 
-            var tomrrow = now.AddDays(1);
-            var yesterday = now.AddDays(-1);
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: "+ duration);
 
-            Console.WriteLine(now.ToLongDateString());
-            Console.WriteLine(now.ToShortDateString());
-            Console.WriteLine(now.ToLongTimeString());
-            Console.WriteLine(now.ToShortTimeString());
-            Console.WriteLine(now.ToString("yyy-MMM-dddd HH:mm"));
+            //properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
 
+            //add
 
+            Console.WriteLine("Add Example: "+ timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(2)));
 
+            //to string
+           Console.WriteLine("ToString" + timeSpan.ToString());
 
-
+            //parse
+            Console.WriteLine("Parse: " + TimeSpan.Parse("01:02:03"));
             Console.ReadLine();
         }
         
