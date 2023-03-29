c46c6c3ba5878209647bce86143cb36983e97c7c
hasira09 <hasiramahel09@gmail.com>
2023-03-29 12:56:52 +0530

    Reverse with Classes

--- Program.cs
+++ Program.cs
@@ -14,14 +14,18 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Type Your Name:");
             var name = Console.ReadLine();
+            var reversed = ReverseName(name);
+            Console.WriteLine("Reversed Name is " + reversed);
+            Console.ReadLine();
+        }
 
+        public static string ReverseName(string name)
+        {
             var array = new char[name.Length];
             for (var i = name.Length; i > 0; i--)
                 array[name.Length - i] = name[i - 1];
 
-            var reversed = new string(array);
-            Console.WriteLine("Reversed Name is " + reversed);
-            Console.ReadLine();
+            return new string(array);
         }
     }
 }
