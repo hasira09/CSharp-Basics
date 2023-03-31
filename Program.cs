fce140f762ae547814b027c81abf3fbac1476e01
hasira09 <hasiramahel09@gmail.com>
2023-03-31 21:06:09 +0530

    Enums
    Related to Data Structures and Algorithms.

--- Program.cs
+++ Program.cs
@@ -1,52 +1,59 @@
-﻿using ConsoleApp1.Maths;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
+﻿using System;
+using ConsoleApp1.Maths;
 
 namespace ConsoleApp1
 {
-
     class Program
     {
-        struct Car
+        public enum Planets
         {
-            public string Brand;
-            public string Model;
-            public int Year;
-            public float Price;
+            Mercury = 1,
+            Venus,
+            Earth,
+            Mars,
+            Jupyter,
+            Saturn,
+            Uranus,
+            Neptune,
+            Pluto
         }
+
+        public enum PlanetsRadius
+        {
+            Mercury = 2439,
+            Venus = 6051,
+            Earth = 6371,
+            Mars = 3389,
+            Jupyter = 69911,
+            Saturn = 58232,
+            Uranus = 25362,
+            Neptune = 24622,
+            Pluto = 1188
+        }
+
+        public static double Volume(PlanetsRadius radius)
+        {
+            double volume = (4.0 / 3.0) * Math.PI * Math.Pow((int)radius, 3);
+            return volume;
+        }
+
         static void Main(string[] args)
         {
-            Car car1;
+            String name = PlanetsRadius.Earth.ToString();
+            int radius = (int)PlanetsRadius.Earth;
+            double volume = Volume(PlanetsRadius.Earth);
 
-            Console.Write("What is the Car Brand? ");
-            car1.Brand = Console.ReadLine();
+            Console.WriteLine("Planet: " + name);
+            Console.WriteLine("Radius: " + radius + "km");
+            Console.WriteLine("Volume: " + volume + "km^3");
 
-            Console.Write("What is the Car Model? ");
-            car1.Model = Console.ReadLine();
-
-            Console.Write("What is the Car Manufactured Year? ");
-            car1.Year = int.Parse(Console.ReadLine());
-
-            Console.Write("What is the Car Price? ");
-            car1.Price = float.Parse(Console.ReadLine());
-
-            Console.WriteLine("The Car Brand is {0} and the Model is {1}. The Manufactured Year of the Car is {2} and the Price is {3}.",car1.Brand, car1.Model, car1.Year, car1.Price);
             Console.ReadLine();
-
         }
-
-        
-           
     }
 }
 
-//Structs are Customer data Types
-//As an Example - Defining a Car
-// * Brand - string
-// * Model - string
-// * Year - int
-// * Price - float
+//Enums are special "class" that contains a set of named integer constants.
+//Enums can be used when you have values that you know will not change.
+//To get the integer value from an item, you must explicitly convert to an int.
+
+//  name = integer
