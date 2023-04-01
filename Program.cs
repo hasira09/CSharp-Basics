97583c92ea903697bed465ff59427231d1295e24
hasira09 <hasiramahel09@gmail.com>
2023-04-01 14:20:31 +0530

    Lists

--- Program.cs
+++ Program.cs
@@ -1,59 +1,52 @@
 ﻿using System;
-using ConsoleApp1.Maths;
+using System.Collections.Generic;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        public enum Planets
-        {
-            Mercury = 1,
-            Venus,
-            Earth,
-            Mars,
-            Jupyter,
-            Saturn,
-            Uranus,
-            Neptune,
-            Pluto
-        }
-
-        public enum PlanetsRadius
-        {
-            Mercury = 2439,
-            Venus = 6051,
-            Earth = 6371,
-            Mars = 3389,
-            Jupyter = 69911,
-            Saturn = 58232,
-            Uranus = 25362,
-            Neptune = 24622,
-            Pluto = 1188
-        }
-
-        public static double Volume(PlanetsRadius radius)
-        {
-            double volume = (4.0 / 3.0) * Math.PI * Math.Pow((int)radius, 3);
-            return volume;
-        }
-
         static void Main(string[] args)
         {
-            String name = PlanetsRadius.Earth.ToString();
-            int radius = (int)PlanetsRadius.Earth;
-            double volume = Volume(PlanetsRadius.Earth);
+            List<string> food = new List<string>();
 
-            Console.WriteLine("Planet: " + name);
-            Console.WriteLine("Radius: " + radius + "km");
-            Console.WriteLine("Volume: " + volume + "km^3");
+            food.Add("Kottu");
+            food.Add("Fried-Rice");
+            food.Add("Pizza");
+            food.Add("Submarines");
+
+            //Removing from the List
+            //food.Remove("Pizza");
+
+            //Adding an Element
+            //food.Insert(0, "Biriyani");
+
+            //Accessing with the index
+            //Console.WriteLine(food[1]);
+            //Console.WriteLine(food.IndexOf("Submarines"));
+            //food.Sort();
+            //food.Reverse();
+            //food.Clear();
+
+            String[] foodArray = food.ToArray();
+
+
+            foreach (String item in foodArray)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Current size of the array
+            //Console.WriteLine(food.Count);
+
+            //Console.WriteLine(food.LastIndexOf("Pizza"));
+            //Console.WriteLine(food.Contains("Submarine"));
+            
 
             Console.ReadLine();
         }
     }
 }
 
-//Enums are special "class" that contains a set of named integer constants.
-//Enums can be used when you have values that you know will not change.
-//To get the integer value from an item, you must explicitly convert to an int.
-
-//  name = integer
+//Lists is a Data Structure that represents a list of objects that can be accessed by index.
+//Similar to arrays. But can dynamically increase/decrease in size
+//Using System.Collections.Generic;
